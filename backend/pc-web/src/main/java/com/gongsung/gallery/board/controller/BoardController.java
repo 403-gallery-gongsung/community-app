4e4312065d4af37d3859fa433b31828d438ec237
kcm0147 <kcm0147@hanmail.net>
2021-02-14 00:00:57 +0900

    feat: board controller 추가
    <label list>
    
    feat : 새로운 기능에 대한 커밋
    fix : 버그 수정에 대한 커밋
    build : 빌드 관련 파일 수정에 대한 커
    chore : 그 외 자잘한 수정에 대한 커밋
    ci : CI관련 설정 수정에 대한 커밋
    docs : 문서 수정에 대한 커밋
    style : 코드 스타일 혹은 포맷 등에 관한 커밋
    refactor :  코드 리팩토링에 대한 커밋
    test : 테스트 코드 수정에 대한 커밋

---
+++ backend/pc-web/src/main/java/com/gongsung/gallery/board/controller/BoardController.java
@@ -0,0 +1,59 @@
+package com.gongsung.gallery.board.controller;
+
+import com.gongsung.gallery.Board;
+import com.gongsung.gallery.User;
+
+
+import com.gongsung.gallery.board.service.BoardService;
+import com.gongsung.gallery.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class BoardController {
+
+    @Autowired
+    private BoardService boardService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/board/boardList/{category}")
+    public String boardList(@PathVariable("category") String category, Model model){
+
+        List<Board> boards = boardService.scanBoard(category);
+        model.addAttribute("boards",boards);
+        return "board/boardList";
+    }
+
+    @GetMapping("/board/{userId}/writeBoard")
+    public String craeteBoard(@PathVariable("userId") Long userId,Model model){
+
+        model.addAttribute("boardForm",new Board());
+        model.addAttribute("userId",userId);
+
+        return "board/writeBoard";
+    }
+
+    @PostMapping("/board/{userId}/writeBoard")
+    public String writeBoard(@PathVariable("userId") Long userId,Board boardForm){
+
+        User user = userRepository.find(userId);
+        Board board = boardService.createBoard(user);
+        boardService.write(board,boardForm.getContent(),boardForm.getTitle(),boardForm.getCategory());
+
+        return "redirect:/board/boardList/all";
+    }
+
+
+}
